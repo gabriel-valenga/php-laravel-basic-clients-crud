add72b784064fdba63c1210af3befa1654e48a94
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-07-21 08:41:12 -0300

    feat: Creating client table and client controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+
+Route::resource('clients', ClientController::class);
 
 Route::get('/', function () {
     return view('welcome');
